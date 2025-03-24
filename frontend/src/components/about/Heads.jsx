7184077280674dae29e178b6c960e55f3ac079b8
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2025-03-24 18:02:23 +0530

    component showcasing heads added

--- frontend/src/components/about/Heads.jsx
+++ frontend/src/components/about/Heads.jsx
@@ -28,26 +28,30 @@ import { memo } from "react";
 import Image from "next/image";
 
 const Heads = ({ teamData = [] }) => {
-    console.log("Heads Prop:", teamData); 
+    console.log("Team Prop:", teamData); 
 
     if (!teamData.length) {
         return <p>No team members found.</p>;
     }
 
-    const heads = teamData.filter((member)=>(member.head===true));
+    const headCommittee = teamData.filter((teamMember)=>(teamMember.committee==="head"));
+    console.log("headCommittee Prop:", headCommittee);
+
+    const heads = headCommittee[0].members;
+    console.log("Heads Prop:", heads);
 
     return (
-        <div className="flex flex-col justify-center items-center">
-            <h1>HEADS</h1>
-            <div className={`flex ${heads.length < 3 ? "flex-row justify-around" : "grid grid-cols-3"}`}>
+        <div className="flex flex-col justify-center items-center my-30">
+            <h1 className="text-5xl font-medium">HEADS</h1>
+            <div className={`${heads.length < 3 ? "flex flex-row justify-around" : "grid grid-cols-3"}`}>
                 {
-                    
                     heads.map((head)=>(
-                        <div key={head.id} className="mx-30">
-                            <Image src={head.image} alt={head.name} width={100} height={100}/>
-                            <h2>{head.name}</h2>
-                            <p>{head.position}</p>
-                            <a href={head.linkedin}><button>CONNECT</button></a>
+                        <div key={head.id} className={`${heads.length < 3 ? "mx-40 mt-15 text-center" : "mx-15 mt-15 text-center"}`}>
+                            <Image src={head.image} alt={head.name} width={250} height={250}/>
+                            <h2 className="text-3xl font-medium mt-3">{head.name}</h2>
+                            <p className="text-lg font-light">{head.position}</p>
+                            <p className="text-md font-light">{head.dept}</p>
+                            <a href={head.linkedin}><button className="border-[1px] rounded-sm h-8 w-25 mt-3 hover:text-white hover:bg-black">CONNECT</button></a>
                         </div>
                     ))
                 }
