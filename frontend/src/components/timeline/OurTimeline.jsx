bfc0d02713b3390fa9009811324ed1990e257b02
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2025-03-28 19:21:36 +0530

    timeline page animated

--- frontend/src/components/timeline/OurTimeline.jsx
+++ frontend/src/components/timeline/OurTimeline.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import SlideIn from "../slidein/SlideIn";
 
 export const dynamic = "force-static";
 
@@ -23,20 +24,24 @@ const OurTimeline = async()=>{
 
     return(
         <div className="relative mx-auto px-30 mt-25 mb-35">
-            <h1 className="font-semibold text-6xl text-center mb-15">WE TIMED THEM</h1>
+            <SlideIn direction="down" delay={200}>
+                <h1 className="font-semibold text-6xl text-center mb-15">WE TIMED THEM</h1>
+            </SlideIn>
             <div className="absolute left-1/2 transform -translate-x-1 bg-black w-[2px] h-full">
             </div>
             {events.map((event) => (
             <div key={event.id} className={`mt-15 flex ${event.id % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}>
                 <div className="w-1/2 flex justify-center">
-                    <div className="w-[450px]">
-                        <p className="text-xl font-light">{event.date}</p>
-                        <h2 className="text-4xl font-semibold my-2">{event.title}</h2>
-                        <p className="mb-3">{event.description}</p>
-                        <Image src={event.image} alt={event.id} width={450} height={200}/>
-                    </div>
-                    </div>
-                    <div className="w-1/2">
+                    <SlideIn direction={`${event.id%2==0 ? 'right' : 'left'}`}>
+                        <div className="w-[450px]">
+                            <p className="text-xl font-light">{event.date}</p>
+                            <h2 className="text-4xl font-semibold my-2">{event.title}</h2>
+                            <p className="mb-3">{event.description}</p>
+                            <Image src={event.image} alt={event.id} width={450} height={200}/>
+                        </div>
+                    </SlideIn>
+                </div>
+                <div className="w-1/2">
                 </div>
             </div>
             ))}
