3c663923ff5126942d5a9a88c639198e0229344e
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2025-03-28 18:16:51 +0530

    missions page animated

--- frontend/src/components/missions/UpcomingProjects.jsx
+++ frontend/src/components/missions/UpcomingProjects.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from "react"
+import SlideIn from "../slidein/SlideIn";
 
 export default function UpcomingProjects(){
     const [projects, setProjects] = useState([]);
@@ -24,17 +25,21 @@ export default function UpcomingProjects(){
 
     return(
         <div className="pb-20 pt-5">
-            <h1 className="text-center text-7xl font-medium py-10">YET TO BE LAUNCHED</h1>
+            <SlideIn direction="down" delay={200}>
+                <h1 className="text-center text-7xl font-medium py-10">YET TO BE LAUNCHED</h1>
+            </SlideIn>
             <div className="grid grid-cols-3 gap-14 mx-15">
                 {
                     projects.map((project)=>(
-                        <div key={project.id} className="shadow-xl">
-                            <img src={project.image} className="h-60"/>
-                            <div className="p-5 h-35">
-                                <h2 className="text-2xl font-medium">{project.title}</h2>
-                                <p className="text-sm">{project.description}</p>
+                        <SlideIn direction="up" key={project.id}>
+                            <div className="shadow-xl">
+                                <img src={project.image} className="h-60"/>
+                                <div className="p-5 h-35">
+                                    <h2 className="text-2xl font-medium">{project.title}</h2>
+                                    <p className="text-sm">{project.description}</p>
+                                </div>
                             </div>
-                        </div>
+                        </SlideIn>
                     ))
                 }
             </div>
